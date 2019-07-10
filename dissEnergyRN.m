fd2046936519e4e5286eb41b45750340c8fb23b6
awei1995 <44083706+awei1995@users.noreply.github.com>
2019-07-11 07:52:35 +0800

    0711

--- dissEnergyRN.m
+++ dissEnergyRN.m
@@ -53,16 +53,24 @@ function Model = dissEnergyRN(Model, roundArch, netArch)
             energy = Model.nodeArch.node(i).energy;
 
             % energy for aggregation & Rx 
-            energy = energy - (packetLength * ERX * Model.nodeArch.node(i).child + ...
-                     packetLength * EDA * (Model.nodeArch.node(i).child + 1));
+%             energy = energy - (packetLength * ERX * Model.nodeArch.node(i).child + ...
+%                      packetLength * EDA * (Model.nodeArch.node(i).child + 1));
+            energy = energy - (packetLength * ERX * Model.nodeArch.node(i).child);      
 
             % energy for transferring
+%             if(Dist >= d0)
+%                  Model.nodeArch.node(i).energy = energy - ...
+%                      (ETX * packetLength + Emp * packetLength  * (Dist ^ 4));
+%             else
+%                  Model.nodeArch.node(i).energy = energy - ...
+%                      (ETX * packetLength + Efs * packetLength  * (Dist ^ 2));
+%             end
             if(Dist >= d0)
                  Model.nodeArch.node(i).energy = energy - ...
-                     (ETX * packetLength + Emp * packetLength  * (Dist ^ 4));
+                     (ETX * packetLength + Emp * packetLength * Model.nodeArch.node(i).child * (Dist ^ 4));
             else
                  Model.nodeArch.node(i).energy = energy - ...
-                     (ETX * packetLength + Efs * packetLength  * (Dist ^ 2));
+                     (ETX * packetLength + Efs * packetLength * Model.nodeArch.node(i).child * (Dist ^ 2));
             end
         end
     end
